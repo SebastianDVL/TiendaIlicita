4aaca3dc517a15ea5fa176ab79d0b0fb9800ca30
Sebas <sebas.cay@gmail.com>
2022-05-01 20:17:35 -0500

    v2

--- TiendaIlicita/App_Start/BundleConfig.cs
+++ TiendaIlicita/App_Start/BundleConfig.cs
@@ -19,11 +19,11 @@ namespace TiendaIlicita
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new Bundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
